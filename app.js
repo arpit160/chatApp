e6b95ceff22fee4d35b98bf6bb17fde4e582afba
arpit160 <singhalarpit160@gmail.com>
2021-02-13 13:21:20 +0530

    databse task done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ http=require('http')
 moment=require('moment')
 engine = require('ejs-mate')
 bodyParser = require('body-parser');
+const { nextTick } = require('process');
+const {Content}=require('./models/chat')
 app=express();
 server=http.createServer(app)
 let currentuser;
@@ -45,9 +47,20 @@ io=socketio(server);
 console.log('apple');
 io.on('connection',(socket)=>
 {
-    
-    socket.on('inform',(data)=>
+    let msg_arr=[]
+    socket.on('inform',async (data)=>
     {
+        msgs=await Content.findOne({username:data.username,room:data.room})
+        console.log('msgs are' ,msgs);
+        if(msgs)
+        {
+        msgs=msgs.content;
+        for(let i=0;i<msgs.length;i++)
+        {
+            msg_arr.push(JSON.stringify(msgs[i]));
+            console.log('i is',i)
+            socket.emit('write',[msgs[i].actual_msg,msgs[i].date,msgs[i].space,msgs[i].by])
+        }}
         date=moment().format('h:mm a')
         console.log('inside inform')
         console.log('data',data)
@@ -55,12 +68,15 @@ io.on('connection',(socket)=>
         let roomusers=getroomusers(data.room);
         console.log('roomusers',roomusers)
         socket.join(data.room)
-        socket.emit('write',[`Welcome (${data.username}) to our chat application`,date,'center','Admin'])
-        socket.broadcast.to(data.room).emit('write',[`<b>${data.username}</b> has joined the chat`,date,'center','Admin'])
+        if(msg_arr.length==0)
+        {
+            socket.emit('write',[`<center>Welcome (${data.username}) to our chat application</center>`,date,'center','Admin'])
+        }
+        socket.broadcast.to(data.room).emit('write',[`<center><b>${data.username}</b> has joined the chat</center>`,date,'center','Admin'])
         io.to(data.room).emit('updatelist',roomusers)
-       
+        
     })
-
+     
     socket.on('message-sent',(message)=>
     {
         date=moment().format('h:mm a')
@@ -71,7 +87,38 @@ io.on('connection',(socket)=>
         socket.broadcast.to(currentuser.room).emit('write',[message,date,'left',currentuser.username]);
     })
 
+     socket.on('save',async(arr)=>
+     {
+        let currentuser=getcurrentuser(socket.id);
+        for(let i=0;i<arr.length;i++)
+        {
+            msg_arr.push(arr[i]);
+        }
 
+        let brr=[]
+        for(let i=0;i<msg_arr.length;i++)
+        {
+            brr.push(JSON.parse(msg_arr[i]));
+        }
+        msg_arr=brr;
+        console.log('msg_arr',msg_arr)
+        try{
+            user_content=await Content.findOne({username:currentuser.username,room:currentuser.room})
+            update_data={username:currentuser.username,room:currentuser.room,content:msg_arr}
+            if(user_content)
+            {
+                await Content.updateOne({username:currentuser.username,room:currentuser.room},update_data)
+            }
+            else{
+                a=new Content(update_data)
+                await a.save()
+            }
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+     })
     socket.on('disconnect',()=>
     {
         date=moment().format('h:mm a')
@@ -82,8 +129,9 @@ io.on('connection',(socket)=>
         console.log('roomusersexcept',roomusersexcept)
         removeuser(socket.id)
         print();
-        socket.broadcast.to(currentuser.room).emit('write',[`<b>${currentuser.username}</b> left the chat`,date,'center','Admin'])
-        socket.broadcast.to(currentuser.room).emit('updatelist',roomusersexcept)
+        socket.broadcast.to(currentuser.room).emit('write',[`<center><b>${currentuser.username}</b> left the chat</center>`,date,'center','Admin'])
+        socket.broadcast.to(currentuser.room).emit('updatelist',roomusersexcept);
+        
     })
 })
 
--- models/chat.js
+++ models/chat.js
@@ -4,7 +4,13 @@ mongoose = require('mongoose');
 const msgschema=new mongoose.Schema({
     username:String,
     room:String,
-    content:String 
+    content:
+    [{
+        by:String,
+        date:String,
+        actual_msg:String,
+        space:String
+    } ]
 })
 
 Content = mongoose.model('Content',msgschema)
