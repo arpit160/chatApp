508dda940704e54963b61e788f0d9aa4e5110ea3
arpit160 <singhalarpit160@gmail.com>
2021-03-07 17:39:36 +0530

    added background image

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const { nextTick } = require('process');
 const {Content}=require('./models/chat')
 app=express();
 server=http.createServer(app)
-let currentuser;
 
 mongoose.connect('mongodb://localhost:27017/chatapp', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>
 {
@@ -23,6 +22,7 @@ app.use(bodyParser.json())
 app.engine('ejs', engine);
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const{
     bindinfo,
